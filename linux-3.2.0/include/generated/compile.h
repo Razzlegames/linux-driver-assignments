2ca6f2e1c9b539cd3b2fd0110b24d3babc20b9ab
razzlegames@gmail.com <razzlegames@gmail.com@d6de6a3d-b9b5-1c46-e522-653ac37c8e2a>
2013-03-07 02:11:43 +0000

    Init checkins

---
+++ linux-3.2.0/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 3 */
+/* SMP */
+#define UTS_MACHINE "x86_64"
+#define UTS_VERSION "#3 SMP Sat Mar 2 11:51:35 MST 2013"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "kyle-VirtualBox"
+#define LINUX_COMPILER "gcc version 4.6.3 (Ubuntu/Linaro 4.6.3-1ubuntu5) "
