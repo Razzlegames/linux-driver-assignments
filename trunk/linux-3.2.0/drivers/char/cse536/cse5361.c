ea2f2a969a8f318e5c556d0755a50c0427ed2be6
razzlegames@gmail.com <razzlegames@gmail.com@d6de6a3d-b9b5-1c46-e522-653ac37c8e2a>
2013-03-23 00:51:29 +0000

    Adding some code for IP raw packets... need to test

--- trunk/linux-3.2.0/drivers/char/cse536/cse5361.c
+++ trunk/linux-3.2.0/drivers/char/cse536/cse5361.c
@@ -19,33 +19,49 @@
 #include <net/protocol.h>
 #include <net/udp.h>
 #include <linux/in.h>
+#include <linux/skbuff.h>
+#include <linux/ip.h>
 
 /// Attempt to use an unreserved IP protocol number
 #define IPPROTO_CSE536  253
 #define CSE536_MAJOR 234
+
+#define ERROR(s, ...) \
+  printk(KERN_ERR "%s:%d ERROR: " s, __FILE__, __LINE__, ##__VA_ARGS__); \
 
 static int debug_enable = 0;
 module_param(debug_enable, int, 0);
 MODULE_PARM_DESC(debug_enable, "Enable module debug mode.");
 struct file_operations cse536_fops;
 
-#define ERROR(s, ...) \
-  printk(KERN_ERR "%s:%d ERROR: " s, __FILE__, __LINE__, ##__VA_ARGS__); \
+static void send(unsigned int data_size, const char* SADDR_STRING,
+    const char* DADDR_STRING);
 
 static void cse536_err(struct sk_buff *skb, u32 info);
 
+//************************************************************************
+static const struct net_protocol cse536_protocol = {
+	.handler	=	xfrm4_rcv,
+	.err_handler	=	cse536_err,
+	.no_policy	=	1,
+	.netns_ok	=	1,
+};
+
+//************************************************************************
 static int cse536_open(struct inode *inode, struct file *file)
 {
   printk("cse536_open: successful\n");
   return 0;
 }
 
+//************************************************************************
 static int cse536_release(struct inode *inode, struct file *file)
 {
   printk("cse536_release: successful\n");
   return 0;
 }
 
+//************************************************************************
 static ssize_t cse536_read(struct file *file, char *buf, size_t count,
     loff_t *ptr)
 {
@@ -55,6 +71,7 @@ static ssize_t cse536_read(struct file *file, char *buf, size_t count,
   return retCount;
 }
 
+//************************************************************************
 static ssize_t cse536_write(struct file *file, const char *buf,
     size_t count, loff_t * ppos)
 {
@@ -62,19 +79,14 @@ static ssize_t cse536_write(struct file *file, const char *buf,
   return 0;
 }
 
+//************************************************************************
 static long cse536_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 {
   printk("cse536_ioctl: cmd=%d, arg=%ld\n", cmd, arg);
   return 0;
 }
 
-static const struct net_protocol cse536_protocol = {
-	.handler	=	xfrm4_rcv,
-	.err_handler	=	cse536_err,
-	.no_policy	=	1,
-	.netns_ok	=	1,
-};
-
+//************************************************************************
 static int __init cse536_init(void)
 {
 
@@ -106,12 +118,14 @@ static int __init cse536_init(void)
 
 }
 
+//************************************************************************
 static void __exit cse536_exit(void)
 {
   unregister_chrdev(CSE536_MAJOR, "cse5361"); 
   printk("cse536 module Exit\n");
 }
 
+//************************************************************************
 struct file_operations cse536_fops = {
 owner: THIS_MODULE,
        read: cse536_read,
@@ -121,6 +135,52 @@ owner: THIS_MODULE,
        release: cse536_release,
 };
 
+//************************************************************************
+/**
+ *  Send out a raw packet using the IP layer
+ */
+static void send(unsigned int data_size, const char* SADDR_STRING,
+    const char* DADDR_STRING)
+{
+
+  const int LENGTH = 1500 + sizeof(struct iphdr);
+  struct sk_buff* skb = alloc_skb(LENGTH, GFP_ATOMIC);
+  struct iphdr* ip_header = NULL;
+
+  if(skb == NULL)
+  {
+
+    ERROR("Could not allocate sk_buff!\n");
+    return;
+  }
+
+  skb_put(skb, LENGTH);
+
+  skb_reset_transport_header(skb);
+
+  skb_push(skb, sizeof(*ip_header));
+  skb_reset_network_header(skb);
+  ip_header = ip_hdr(skb);
+
+  ip_header->version = 4;
+  ip_header->ihl = 5;
+  ip_header->tos = 0;
+  ip_header->tot_len = htons(data_size + sizeof(ip_header));
+  ip_header->id = 0;
+  ip_header->frag_off = 0;
+  ip_header->ttl = 64;
+  ip_header->protocol = IPPROTO_CSE536;
+  ip_header->check = ip_fast_csum((unsigned char*)ip_header,
+      ip_header->ihl);
+
+  ip_header->saddr = in_aton(SADDR_STRING);
+  ip_header->daddr = in_aton(DADDR_STRING);
+
+  ip_local_out(skb);
+
+}
+
+//***************************************************************
 static void cse536_err(struct sk_buff *skb, u32 info)
 {
 
