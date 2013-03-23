2e4f8837aa3428e0dfd9894b1538f6bfb8b3e615
razzlegames@gmail.com <razzlegames@gmail.com@d6de6a3d-b9b5-1c46-e522-653ac37c8e2a>
2013-03-23 21:29:13 +0000

    experimenting

--- trunk/cse536app.c
+++ trunk/cse536app.c
@@ -17,19 +17,21 @@ int main()
   char buffer[128];
   size_t count;
 
-  fd = fopen("/dev/cse5361", "rwb");
+  fd = fopen("/dev/cse5361", "wb");
   if (!fd || ferror(fd))
   { 
     printf("File error opening file:%s\n",
         strerror(errno));
     exit(1);
   }
-  //  count = fread(buffer, 1, sizeof(buffer), fd);
-  //  if (!count)
-  //      printf("No data read\n");
-  //  else
-  //      printf("%s\n", buffer);
-  count = fwrite(buffer, 1, sizeof(buffer), fd);
+  count = fread(buffer, 1, sizeof(buffer), fd);
+  if (!count)
+      printf("No data read\n");
+  else
+      printf("%s\n", buffer);
+
+  char testbuff[] = "testing";
+  count = fwrite(testbuff, 1, sizeof(testbuff), fd);
   if(count <= 0)
   {
     if(ferror(fd))
