b0c4df85d26f2421edbd3907c26ae4d9f58bc499
razzlegames@gmail.com <razzlegames@gmail.com@d6de6a3d-b9b5-1c46-e522-653ac37c8e2a>
2013-03-23 15:58:45 +0000

    Deleting some generated files

--- trunk/linux-3.2.0/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 6 */
-/* SMP */
-#define UTS_MACHINE "x86_64"
-#define UTS_VERSION "#6 SMP Fri Mar 22 19:46:08 MST 2013"
-#define LINUX_COMPILE_BY "root"
-#define LINUX_COMPILE_HOST "kyle2-Desktop"
-#define LINUX_COMPILER "gcc version 4.6.3 (Ubuntu/Linaro 4.6.3-1ubuntu5) "
