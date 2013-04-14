90f396201bc38ef5a4d2d9c8122933fde954619d
razzlegames@gmail.com <razzlegames@gmail.com@d6de6a3d-b9b5-1c46-e522-653ac37c8e2a>
2013-04-14 00:52:02 +0000

    Starting work on ack for protocol

--- trunk/linux-3.2.0/drivers/char/cse536/cse5361.h
+++ trunk/linux-3.2.0/drivers/char/cse536/cse5361.h
@@ -2,11 +2,48 @@
 #ifndef CSE5361_H
 #define CSE5361_H 
 
+#include <linux/types.h>
+
 enum
 {
   IP_RECORD=1, DATA_RECORD=2,
 };
 
 
+typedef enum 
+{
+
+  UNKNOWN_MESSAGE = -1,
+  ACK_MESSAGE = 0,
+  EVENT_MESSAGE = 1,
+}MessageType;
+
+typedef struct 
+{
+  /// Address we will receive ack from
+  __be32 rec_address;
+
+  /// The counter value we had when we sent the event 
+  ///   Message (the ack will have this)
+  unsigned int counter;
+
+}MessageAckRecord;
+
+
+typedef struct 
+{
+  /// Record ID
+  unsigned int record_id;
+
+  /// Source IP
+  __be32 source_ip;
+
+  /// counter
+  unsigned int counter;
+
+  /// data
+  unsigned char data[244];
+
+}EventMessage;
 
 #endif /* CSE5361_H */
